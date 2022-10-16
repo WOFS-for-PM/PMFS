8b9ce87c3ba8c0baca54e05860d3ecc5c1afb91e
deadpool <2841954631@qq.com>
2022-10-16 22:12:31 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ ! "$1" ]; then
+    dev=/dev/pmem0
+else
+    dev=$1
+fi
+
+if [ ! "$2" ]; then
+    mnt=/mnt/pmem0
+else
+    mnt=$2
+fi
+
+if [ ! "$3" ]; then
+    make
+else 
+    make "$3"
+fi
+
+sudo umount "$mnt"
+sudo rmmod pmfs
+sudo insmod pmfs.ko measure_timing=1
+
+# sleep 1
+
+sudo mount -t pmfs -o init "$dev" "$mnt"
+
+#cp test1 /mnt/ramdisk/
+#dd if=/dev/zero of=/mnt/ramdisk/test1 bs=1M count=1024 oflag=direct
